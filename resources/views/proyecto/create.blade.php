b84dab9285e79e0c111565f518ed4242b6103c29
yaviracjorge <jlr.corella@yavirac.edu.ec>
2025-06-22 19:42:40 -0500

    correcion visual

--- resources/views/proyecto/create.blade.php
+++ resources/views/proyecto/create.blade.php
@@ -53,7 +53,7 @@
 
             <!-- Tiempo de dedicación -->
             <div>
-                <label for="tiempo_dedicacion" class="block text-sm font-medium text-gray-700 mb-1">Tiempo Dedicación:</label>
+                <label for="tiempo_dedicacion" class="block text-sm font-medium text-gray-700 mb-1">Horas de Dedicación:</label>
                 <input type="text" id="tiempo_dedicacion" name="tiempo_dedicacion" required
                     class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
                     placeholder="Ej: T100, T60, etc.">
